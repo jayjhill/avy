5db5b7428f223de02ffcc9a192a874c131b0c916
jjhi11 <jayhill@utah.gov>
2021-02-20 09:52:33 -0700

    init

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Switching to branch master"
+git checkout master
+
+echo "Building app"
+npm run build
+
+echo "Deploying files to server"
+rsync -avP build/ jj@mountainlabmaps.com:/var/www/mountainlabmaps.com/public_html
+echo "Deployment complete"
