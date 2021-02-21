95553a8fab0cc6c1dfb4e9ecae6af6e545ccb0c5
jayjhill <hill.jay@gmail.com>
2021-02-20 21:34:07 -0700

    trying

--- src/App.js
+++ src/App.js
@@ -22,56 +22,56 @@ function App() {
   );
 }
 
-const Nightmare = require('nightmare');
-const cheerio = require('cheerio');
+// const Nightmare = require('nightmare');
+// const cheerio = require('cheerio');
 
-const Xvfb = require('xvfb');
-let xvfb = new Xvfb();
+// const Xvfb = require('xvfb');
+// let xvfb = new Xvfb();
 
-try {
-  xvfb.startSync();
-}
-catch (e) {
-  console.log(e);
-}
+// try {
+//   xvfb.startSync();
+// }
+// catch (e) {
+//   console.log(e);
+// }
 
-const nightmare = Nightmare({ show: true })
-const url = 'https://utahavalanchecenter.org/forecast/salt-lake';
+// const nightmare = Nightmare({ show: true })
+// const url = 'https://utahavalanchecenter.org/forecast/salt-lake';
 
-nightmare
-    .goto(url)
-    .wait('body')
-    .evaluate(() => document.querySelector('body').innerHTML)
-    .end()
-    .then(response => {
-        console.log("poop1");
-        console.log(getData("response" + response));
+// nightmare
+//     .goto(url)
+//     .wait('body')
+//     .evaluate(() => document.querySelector('body').innerHTML)
+//     .end()
+//     .then(response => {
+//         console.log("poop1");
+//         console.log(getData("response" + response));
 
-    }).catch(err=> {
-        console.log(err);
-    });
+//     }).catch(err=> {
+//         console.log(err);
+//     });
 
 
-    let getData = html => {
-        console.log("poop");
-        //data = [];
-        const $ = cheerio.load(html);
-        var title = $('.text_03.pt2');
-        //console.log(title.text());
-        let report = title.text();
-//parse report for keywords
-        let high = report.match(/high/gi);
-        let allAspects = report.match(/all steep/gi);
-        let mid = report.match(/mid/gi);
-        let upper = report.match(/upper/gi);
-        let low = report.match(/low/gi);
-        console.log(high);
-        console.log(low);
-        console.log(mid);
-        console.log(upper);
-        console.log(allAspects);
+//     let getData = html => {
+//         console.log("poop");
+//         //data = [];
+//         const $ = cheerio.load(html);
+//         var title = $('.text_03.pt2');
+//         //console.log(title.text());
+//         let report = title.text();
+// //parse report for keywords
+//         let high = report.match(/high/gi);
+//         let allAspects = report.match(/all steep/gi);
+//         let mid = report.match(/mid/gi);
+//         let upper = report.match(/upper/gi);
+//         let low = report.match(/low/gi);
+//         console.log(high);
+//         console.log(low);
+//         console.log(mid);
+//         console.log(upper);
+//         console.log(allAspects);
 
-        return report;
-    }
+//         return report;
+//     }
 
 export default App;
